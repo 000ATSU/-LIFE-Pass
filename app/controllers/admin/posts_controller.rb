49b10a857cada5273abdb7a280b67cd4ebc67553
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-08-06 14:27:09 +0000

    finish layout

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
+
   def show
     @post = Post.find(params[:id])
   end
@@ -7,7 +8,6 @@ class Admin::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    # admin_user_pathにリダイレクトするとエラー発生
     redirect_to admin_users_path
   end
 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,13 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @users = User.page(params[:page]).per(4)
+    @users = User.order(created_at: :desc).page(params[:page]).per(8)
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page]).per(1)
+    @posts = @user.posts.page(params[:page]).per(6)
+    @get_favorite = Favorite.where(post_id: @posts.ids)
   end
 
 end
