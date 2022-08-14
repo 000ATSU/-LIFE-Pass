d01970d165f86b92204551e1941357a5812fe391
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-08-14 13:10:51 +0000

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,10 @@ Admin.create!(
   ]
   )
 
-User.create!(
-  [
-  {email: 'user1@user.com', password: 'user1@user.com', user_name: 'user1'},
-  {email: 'user2@user.com', password: 'user2@user.com', user_name: 'user2'},
-  {email: 'user3@user.com', password: 'user3@user.com', user_name: 'user3'},
-  {email: 'user4@user.com', password: 'user4@user.com', user_name: 'user4'},
-  {email: 'user5@user.com', password: 'user5@user.com', user_name: 'user5'},
-  {email: 'user6@user.com', password: 'user6@user.com', user_name: 'user6'},
-  ]
+6.times do |n|
+  User.create!(
+    [
+    {email: 'user#{n}@user.com', password: 'user#{n}@user.com', user_name: 'user#{n}'}
+    ]
   )
+end
