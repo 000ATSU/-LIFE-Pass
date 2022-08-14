98364059d511f7942a083ee06dcd23a932e2126b
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-08-14 12:56:42 +0000

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ Admin.create!(
 
 User.create!(
   [
-  {email: 'user1@user.com', password: 'user1@user.com'},
-  {email: 'user2@user.com', password: 'user2@user.com'},
-  {email: 'user3@user.com', password: 'user3@user.com'},
-  {email: 'user4@user.com', password: 'user4@user.com'},
-  {email: 'user5@user.com', password: 'user5@user.com'},
-  {email: 'user6@user.com', password: 'user6@user.com'},
+  {email: 'user1@user.com', password: 'user1@user.com', user_name: 'user1'},
+  {email: 'user2@user.com', password: 'user2@user.com', user_name: 'user2'},
+  {email: 'user3@user.com', password: 'user3@user.com', user_name: 'user3'},
+  {email: 'user4@user.com', password: 'user4@user.com', user_name: 'user4'},
+  {email: 'user5@user.com', password: 'user5@user.com', user_name: 'user5'},
+  {email: 'user6@user.com', password: 'user6@user.com', user_name: 'user6'},
   ]
   )
